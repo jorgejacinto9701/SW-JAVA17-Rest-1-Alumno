8d0e7683a8827b606f5445f4b276a51b8eed8099
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-11 18:50:52 -0500

    inicio

---
+++ src/main/java/com/centroinformacion/SwJava17Rest1ListaProductoApplication.java
@@ -0,0 +1,13 @@
+package com.centroinformacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwJava17Rest1ListaProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwJava17Rest1ListaProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centroinformacion/SwJava17Rest1ListaProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centroinformacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwJava17Rest1ListaProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
